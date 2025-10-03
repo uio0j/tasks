5a41b0b675a1b558f1b54f631a75bfd100750c94
Alaa <alaaali@udel.edu>
2025-10-02 22:52:07 -0400

    changes to multipleChoice and ChangeColor

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,24 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [choice, setChoice] = useState<string>(options[0]);
+    function updateChoice(event: React.ChangeEvent<HTMLSelectElement>) {
+        setChoice(event.target.value);
+    }
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="choice">
+                <Form.Label>What is your choice?</Form.Label>
+                <Form.Select value={choice} onChange={updateChoice}>
+                    {options.map((color: string) => (
+                        <option key={color} value={color}>
+                            {color}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            Your choice is {expectedAnswer === choice ? "✔️" : "❌"}
         </div>
     );
 }
